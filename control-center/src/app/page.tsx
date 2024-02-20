9e667aa51501fc4e4a2e5c4ec6176d6a0689cf2c
Carl Valdemar Ebbesen <ecvebbesen@gmail.com>
2024-02-20 09:33:37 +0100

    add socket io logging to webapp! !

--- control-center/src/app/page.tsx
+++ control-center/src/app/page.tsx
@@ -1,10 +1,11 @@
-import Image from 'next/image'
+import { ManualControl } from "@/components/control/manualControl";
+import { VideoStream } from "@/components/video/videoStream";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <VideoStream/>
-     
+    <main className="flex min-h-screen flex-col items-center justify-between p-5">
+      <VideoStream />
+      <ManualControl />
     </main>
-  )
+  );
 }
--- control-center/src/components/video/videoStream.tsx
+++ control-center/src/components/video/videoStream.tsx
@@ -1,1 +1,23 @@
-const VideoStream = ()=>  
+"use client";
+import React, { useRef, useEffect } from "react";
+
+export const VideoStream = () => {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices
+        .getUserMedia({ video: true })
+        .then((stream) => {
+          if (videoRef.current) {
+            videoRef.current.srcObject = stream;
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, []);
+
+  return <video ref={videoRef} autoPlay />;
+};
